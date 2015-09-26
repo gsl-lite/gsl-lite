b1ebad473b27b1dc98e5e8d04a5f63f9e2f11dec
Martin Moene <martin.moene@gmail.com>
2015-09-26 17:49:46 +0200

    Fix compilation

--- test/gsl-lite.t.cpp
+++ test/gsl-lite.t.cpp
@@ -29,15 +29,15 @@ int main( int argc, char * argv[] )
 }
 
 #if 0
-g++ -std=c++98 -I../include/ -Dgsl_THROW_ON_FAILURE -o gsl-lite.t.exe gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
-g++ -std=c++03 -I../include/ -Dgsl_THROW_ON_FAILURE -o gsl-lite.t.exe gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
-g++ -std=c++0x -I../include/ -Dgsl_THROW_ON_FAILURE -o gsl-lite.t.exe gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
-g++ -std=c++11 -I../include/ -Dgsl_THROW_ON_FAILURE -o gsl-lite.t.exe gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
-g++ -std=c++14 -I../include/ -Dgsl_THROW_ON_FAILURE -o gsl-lite.t.exe gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
+g++ -std=c++98 -I../include/ -Dgsl_FEATURE_THROW_ON_FAILURE=1 -o gsl-lite.t.exe gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
+g++ -std=c++03 -I../include/ -Dgsl_FEATURE_THROW_ON_FAILURE=1 -o gsl-lite.t.exe gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
+g++ -std=c++0x -I../include/ -Dgsl_FEATURE_THROW_ON_FAILURE=1 -o gsl-lite.t.exe gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
+g++ -std=c++11 -I../include/ -Dgsl_FEATURE_THROW_ON_FAILURE=1 -o gsl-lite.t.exe gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
+g++ -std=c++14 -I../include/ -Dgsl_FEATURE_THROW_ON_FAILURE=1 -o gsl-lite.t.exe gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
 
-cl -EHsc -I../include/ -Dgsl_THROW_ON_FAILURE gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
+cl -EHsc -I../include/ -Dgsl_FEATURE_THROW_ON_FAILURE=1 gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
 
-cl -EHsc -I../include/ -Dgsl_THROW_ON_FAILURE -Dgsl_CONFIRM_COMPILATION_ERRORS gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
+cl -EHsc -I../include/ -Dgsl_FEATURE_THROW_ON_FAILURE=1 -Dgsl_CONFIRM_COMPILATION_ERRORS gsl-lite.t.cpp owner.t.cpp not_null.t.cpp && gsl-lite.t.exe --pass
 #endif
 
 // end of file
