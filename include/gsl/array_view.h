fa5b7a1f4a09018eee96aea01060295ae2289f60
Martin Moene <martin.moene@gmail.com>
2015-09-29 15:35:32 +0200

    Add empty array_view.h

---
+++ include/gsl/array_view.h
@@ -0,0 +1,22 @@
+// Copyright 2015 by Martin Moene
+//
+// gsl-lite is based on GSL: Guidelines Support Library,
+// https://github.com/microsoft/gsl
+//
+// This code is licensed under the MIT License (MIT). 
+//
+
+#pragma once
+
+#ifndef GSL_ARRAY_VIEW_H_INCLUDED
+#define GSL_ARRAY_VIEW_H_INCLUDED
+
+#include "config.h"
+
+namespace gsl {
+
+}
+
+#endif // GSL_ARRAY_VIEW_H_INCLUDED
+
+// end of file
