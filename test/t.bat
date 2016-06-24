6fcb72297af2aab8c09d14413bb52572030e216c
Martin Moene <martin.moene@gmail.com>
2016-06-24 23:20:02 +0200

    Update for changed contract violation response control macro

--- test/t.bat
+++ test/t.bat
@@ -1,1 +1,1 @@
-cl -EHsc -I../include/gsl -Dgsl_CONFIG_THROWS_FOR_TESTING gsl-lite.t.cpp assert.t.cpp at.t.cpp not_null.t.cpp owner.t.cpp span.t.cpp string_span.t.cpp util.t.cpp && gsl-lite.t.exe --pass
+cl -EHsc -I../include/gsl -Dgsl_CONFIG_CONTRACT_VIOLATION_THROWS gsl-lite.t.cpp assert.t.cpp at.t.cpp not_null.t.cpp owner.t.cpp span.t.cpp string_span.t.cpp util.t.cpp && gsl-lite.t.exe --pass
