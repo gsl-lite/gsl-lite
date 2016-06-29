6b7baec67d788a6e35be68470af900c9d349ea5f
Martin Moene <martin.moene@gmail.com>
2016-06-29 11:14:21 +0200

    Add tests tagged [.std...] to inspect presence of language/library features

--- test/gsl-lite.t.cpp
+++ test/gsl-lite.t.cpp
@@ -8,6 +8,12 @@
 
 #include "gsl-lite.t.h"
 
+#define gsl_PRESENT( x ) \
+    std::cout << #x << ": " << x << "\n"
+
+#define gsl_ABSENT( x ) \
+    std::cout << #x << ": (undefined)\n"
+
 lest::tests & specification() 
 { 
     static lest::tests tests; 
@@ -17,6 +23,120 @@ lest::tests & specification()
 CASE( "__cplusplus" )
 {
     EXPECT( __cplusplus > 0L );
+}
+
+CASE( "Presence of C++ language features" "[.stdlanguage]" )
+{
+#if gsl_HAVE_AUTO 
+    gsl_PRESENT( gsl_HAVE_AUTO );
+#else    
+    gsl_ABSENT(  gsl_HAVE_AUTO );
+#endif
+
+#if gsl_HAVE_NULLPTR 
+    gsl_PRESENT( gsl_HAVE_NULLPTR );
+#else    
+    gsl_ABSENT(  gsl_HAVE_NULLPTR );
+#endif
+
+#if gsl_HAVE_DEFAULT_FUNCTION_TEMPLATE_ARG 
+    gsl_PRESENT( gsl_HAVE_DEFAULT_FUNCTION_TEMPLATE_ARG );
+#else    
+    gsl_ABSENT(  gsl_HAVE_DEFAULT_FUNCTION_TEMPLATE_ARG );
+#endif
+
+#if gsl_HAVE_ALIAS_TEMPLATE 
+    gsl_PRESENT( gsl_HAVE_ALIAS_TEMPLATE );
+#else    
+    gsl_ABSENT(  gsl_HAVE_ALIAS_TEMPLATE );
+#endif
+
+#if gsl_HAVE_CONSTEXPR_11 
+    gsl_PRESENT( gsl_HAVE_CONSTEXPR_11 );
+#else    
+    gsl_ABSENT(  gsl_HAVE_CONSTEXPR_11 );
+#endif
+
+#if gsl_HAVE_ENUM_CLASS 
+    gsl_PRESENT( gsl_HAVE_ENUM_CLASS );
+#else    
+    gsl_ABSENT(  gsl_HAVE_ENUM_CLASS );
+#endif
+
+#if gsl_HAVE_EXPLICIT_CONVERSION 
+    gsl_PRESENT( gsl_HAVE_EXPLICIT_CONVERSION );
+#else    
+    gsl_ABSENT(  gsl_HAVE_EXPLICIT_CONVERSION );
+#endif
+
+#if gsl_HAVE_INITIALIZER_LIST 
+    gsl_PRESENT( gsl_HAVE_INITIALIZER_LIST );
+#else    
+    gsl_ABSENT(  gsl_HAVE_INITIALIZER_LIST );
+#endif
+
+#if gsl_HAVE_IS_DEFAULT 
+    gsl_PRESENT( gsl_HAVE_IS_DEFAULT );
+#else    
+    gsl_ABSENT(  gsl_HAVE_IS_DEFAULT );
+#endif
+
+#if gsl_HAVE_IS_DELETE 
+    gsl_PRESENT( gsl_HAVE_IS_DELETE );
+#else    
+    gsl_ABSENT(  gsl_HAVE_IS_DELETE );
+#endif
+
+#if gsl_HAVE_NOEXCEPT 
+    gsl_PRESENT( gsl_HAVE_NOEXCEPT );
+#else    
+    gsl_ABSENT(  gsl_HAVE_NOEXCEPT );
+#endif
+}
+
+CASE( "Presence of C++ library features" "[.stdlibrary]" )
+{
+#if gsl_HAVE_ARRAY
+    gsl_PRESENT( gsl_HAVE_ARRAY );
+#else    
+    gsl_ABSENT(  gsl_HAVE_ARRAY );
+#endif
+
+#if gsl_HAVE_CONTAINER_DATA_METHOD
+    gsl_PRESENT( gsl_HAVE_CONTAINER_DATA_METHOD );
+#else    
+    gsl_ABSENT(  gsl_HAVE_CONTAINER_DATA_METHOD );
+#endif
+
+#if gsl_HAVE_SIZED_TYPES
+    gsl_PRESENT( gsl_HAVE_SIZED_TYPES );
+#else    
+    gsl_ABSENT(  gsl_HAVE_SIZED_TYPES );
+#endif
+
+#if gsl_HAVE_SHARED_PTR
+    gsl_PRESENT( gsl_HAVE_SHARED_PTR );
+#else    
+    gsl_ABSENT(  gsl_HAVE_SHARED_PTR );
+#endif
+
+#if gsl_HAVE_UNIQUE_PTR
+    gsl_PRESENT( gsl_HAVE_UNIQUE_PTR );
+#else    
+    gsl_ABSENT(  gsl_HAVE_UNIQUE_PTR );
+#endif
+
+#if gsl_HAVE_TYPE_TRAITS
+    gsl_PRESENT( gsl_HAVE_TYPE_TRAITS );
+#else    
+    gsl_ABSENT(  gsl_HAVE_TYPE_TRAITS );
+#endif
+
+#if _HAS_CPP0X
+    gsl_PRESENT( _HAS_CPP0X );
+#else    
+    gsl_ABSENT(  _HAS_CPP0X );
+#endif
 }
 
 int main( int argc, char * argv[] )
