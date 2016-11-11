b38f948b11573476c27f3e8629d2c1991d457b39
Martin Moene <martin.moene@gmail.com>
2016-11-11 10:16:24 +0100

    Add more flags

--- test/tg-all.bat
+++ test/tg-all.bat
@@ -1,3 +1,4 @@
-@for %%s in ( c++98 c++03 c++11 c++14 c++1z ) do (
+@echo off
+for %%s in ( c++98 c++03 c++11 c++14 c++1z ) do (
     call tg.bat %%s
 )
--- test/tg.bat
+++ test/tg.bat
@@ -1,6 +1,11 @@
+@echo off
+REM set FLAG=-Wsign-conversion
+REM set FLAG=-Wshadow
+set EXTRAFLAGS=%FLAG% -Wpedantic -Wconversion -Wctor-dtor-privacy -Wnon-virtual-dtor -Wcast-align -Woverloaded-virtual -Wold-style-cast
+
 @setlocal
 @set std=%1
 @if "%std%"=="" set std=c++11
-g++ -std=%std% -O2 -Wall -Wextra -Wno-unused-parameter -fno-elide-constructors -o gsl-lite.t.exe -I../include/gsl -Dgsl_CONFIG_CONTRACT_VIOLATION_THROWS  -Dgsl_FEATURE_EXPERIMENTAL_RETURN_GUARD gsl-lite.t.cpp assert.t.cpp at.t.cpp byte.t.cpp issue.t.cpp not_null.t.cpp owner.t.cpp span.t.cpp string_span.t.cpp util.t.cpp && gsl-lite.t.exe
+echo GNUC ISO standard: %std%
+g++ -std=%std% -O2 %EXTRAFLAGS% -Wall -Wextra -Wno-unused-parameter -fno-elide-constructors -o gsl-lite.t.exe -I../include/gsl -Dgsl_CONFIG_SPAN_INDEX_TYPE=std::ptrdiff_t -Dgsl_CONFIG_CONTRACT_VIOLATION_THROWS  -Dgsl_FEATURE_EXPERIMENTAL_RETURN_GUARD gsl-lite.t.cpp assert.t.cpp at.t.cpp byte.t.cpp issue.t.cpp not_null.t.cpp owner.t.cpp span.t.cpp string_span.t.cpp util.t.cpp && gsl-lite.t.exe
 @endlocal
-
