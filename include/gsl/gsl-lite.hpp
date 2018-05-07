18788fc0bfc0ad7ca8efcf6ef082a1d6a9a2b8fa
Martin Moene <martin.moene@gmail.com>
2018-05-07 16:34:39 +0200

    Introduce gsl_lite_MAJOR, gsl_lite_MINOR, gsl_lite_PATCH (issue #118)

--- include/gsl/gsl-lite.hpp
+++ include/gsl/gsl-lite.hpp
@@ -31,7 +31,10 @@
 #include <utility>
 #include <vector>
 
-#define  gsl_lite_VERSION "0.30.0"
+#define  gsl_lite_MAJOR     0
+#define  gsl_lite_MINOR    30
+#define  gsl_lite_PATCH     0
+#define  gsl_lite_VERSION  gsl_STRINGIFY(gsl_lite_MAJOR) "." gsl_STRINGIFY(gsl_lite_MINOR) "." gsl_STRINGIFY(gsl_lite_PATCH)
 
 // gsl-lite backward compatibility:
 
