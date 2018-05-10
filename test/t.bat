ef3101e81bb388c85c4516d80d588571e4e72f63
Martin Moene <martin.moene@gmail.com>
2018-05-10 14:42:01 +0200

    Omit macros min(), max() via -DNOMINMAX

--- test/t.bat
+++ test/t.bat
@@ -10,7 +10,7 @@ set CppCoreCheckInclude=%VCINSTALLDIR%\Auxiliary\VS\include
 call :CompilerVersion version
 echo VC%version%: %args%
 
-cl -EHsc -I../include -I"%CppCoreCheckInclude%" %args% -Dgsl_FEATURE_EXPERIMENTAL_RETURN_GUARD -Dgsl_CONFIG_CONTRACT_VIOLATION_THROWS gsl-lite.t.cpp assert.t.cpp at.t.cpp byte.t.cpp issue.t.cpp not_null.t.cpp owner.t.cpp span.t.cpp string_span.t.cpp util.t.cpp && gsl-lite.t.exe
+cl -EHsc -I../include -I"%CppCoreCheckInclude%" %args% -DNOMINMAX -Dgsl_FEATURE_EXPERIMENTAL_RETURN_GUARD -Dgsl_CONFIG_CONTRACT_VIOLATION_THROWS gsl-lite.t.cpp assert.t.cpp at.t.cpp byte.t.cpp issue.t.cpp not_null.t.cpp owner.t.cpp span.t.cpp string_span.t.cpp util.t.cpp && gsl-lite.t.exe
 
 endlocal & goto :EOF
 
@@ -29,5 +29,5 @@ for /f %%x in ('%tmpprogram%') do set version=%%x
 del %tmpprogram%.* >nul
 set offset=0
 if %version% LSS 1900 set /a offset=1
-set /a version="version / 100 - 5 - offset"
+set /a version="version / 10 - 10 * ( 5 + offset )"
 endlocal & set %1=%version%& goto :EOF
