gsl-lite/gsl-lite

commit 643c233003ad4c025a71d9c5612e6ca3bc085a90

Mixed bag of minor improvements (#176)
* Fix #133

* Add missing `constexpr` qualifiers

* Export to build directory and to user package registry

This change permits referencing a local copy of gsl-lite in another
CMake project without having to install it first.

* Minor fixes

include/gsl/gsl-lite.hpp
@@ -25,7 +25,7 @@
 #include <iterator>
 #include <limits>
 #include <memory>
-#include <ostream>
+#include <iosfwd>
 #include <stdexcept>
 #include <string>
 #include <utility>
@@ -847,7 +847,7 @@ struct fail_fast : public std::logic_error
 
 # if gsl_CONFIG( CONTRACT_VIOLATION_THROWS_V )
 
-gsl_api inline void fail_fast_assert( bool cond, char const * const message )
+gsl_api gsl_constexpr inline void fail_fast_assert( bool cond, char const * const message )
 {
     if ( !cond )
         throw fail_fast( message );
@@ -858,7 +858,7 @@ gsl_api inline void fail_fast_assert( bool cond, char const * const message )
 // Should be defined by user
 gsl_api void fail_fast_assert_handler( char const * const expression, char const * const message, char const * const file, int line );
 
-gsl_api inline void fail_fast_assert( bool cond, char const * const expression, char const * const message, char const * const file, int line )
+gsl_api gsl_constexpr inline void fail_fast_assert( bool cond, char const * const expression, char const * const message, char const * const file, int line )
 {
     if ( !cond )
         fail_fast_assert_handler( expression, message, file, line );
@@ -866,7 +866,7 @@ gsl_api inline void fail_fast_assert( bool cond, char const * const expression, 
 
 # else
 
-gsl_api inline void fail_fast_assert( bool cond ) gsl_noexcept
+gsl_api gsl_constexpr inline void fail_fast_assert( bool cond ) gsl_noexcept
 {
 #ifdef __CUDA_ARCH__
     assert(cond);

CMakeLists.txt
@@ -39,6 +39,7 @@ endif()
 
 option( GSL_LITE_OPT_BUILD_TESTS    "Build and perform gsl-lite tests" ${gsl_IS_TOPLEVEL_PROJECT} )
 option( GSL_LITE_OPT_BUILD_EXAMPLES "Build gsl-lite examples" OFF )
+option( CMAKE_EXPORT_PACKAGE_REGISTRY "Export build directory to CMake user package registry" OFF )
 
 # If requested, build and perform tests, build examples:
 
@@ -113,6 +114,18 @@ install(
     NAMESPACE    ${package_nspace}::
     DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${package_folder}"
 )
+
+export(
+    EXPORT       ${package_target}
+    NAMESPACE    ${package_nspace}::
+    FILE         "${CMAKE_CURRENT_BINARY_DIR}/${package_name}-targets.cmake"
+)
+
+if(CMAKE_EXPORT_PACKAGE_REGISTRY)
+    export(
+        PACKAGE  ${package_name}
+    )
+endif()
 
 install(
     FILES       "${CMAKE_CURRENT_BINARY_DIR}/${package_config}"

commit 56aff97cdd4f0321252ce8e62aac0dce1a95588a

Fix compilation issues in current Travis builds (#187) (thanks to @petamas)
* Fix issue #183: Ensures and Expects did not play well with gcc 5

* Try to fix apple-clang builds broken by PR #176 (while resolving issue #133)

* Add <ostream> include to gps-lite.t.hpp, so tests using ostream can be compiled (still trying to fix apple-clang builds broken by PR #176 while resolving issue #133)

* Move <ostream>'s include higher in gps-lite.t.hpp (still trying to fix apple-clang builds broken by PR #176 while resolving issue #133)

include/gsl/gsl-lite.hpp
@@ -25,7 +25,7 @@
 #include <iterator>
 #include <limits>
 #include <memory>
-#include <iosfwd>
+#include <ios>
 #include <stdexcept>
 #include <string>
 #include <utility>
@@ -315,7 +315,7 @@
 
 // Presence of C++14 language features:
 
-#define gsl_HAVE_CONSTEXPR_14           gsl_CPP14_000
+#define gsl_HAVE_CONSTEXPR_14           ( gsl_CPP14_000 && ! gsl_BETWEEN( gsl_COMPILER_GNUC_VERSION, 1, 600 ) )
 #define gsl_HAVE_DECLTYPE_AUTO          gsl_CPP14_140
 
 // Presence of C++17 language features:

test/gsl-lite.t.hpp
@@ -39,6 +39,9 @@
 # pragma GCC   diagnostic ignored "-Wunused-function"
 #endif
 
+// GSL-Lite only depends on <ios>, but we're instantiating templates using streams, so we need <ostream>
+#include <ostream>
+
 namespace lest {
 
 // These functions cannot be found via ADL, so we have to define them before including lest.