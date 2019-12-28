7bea3a38bf3d51a1d2456a5cb8e57cf190a05a4f
Moritz Beutel <mbeutel@users.noreply.github.com>
2019-12-28 04:35:16 +0100

    Disable some tests if required features are not supported

--- test/emulation.t.cpp
+++ test/emulation.t.cpp
@@ -20,25 +20,25 @@
 using namespace gsl;
 
 
-#if gsl_HAVE( TYPE_TRAITS )
+#if gsl_HAVE( TYPE_TRAITS ) && gsl_HAVE( VARIADIC_TEMPLATE )
 template <int> struct True : std::true_type { };
 template <int> struct False : std::false_type { };
 
 struct Incomplete;
-#endif // gsl_HAVE( TYPE_TRAITS )
+#endif // gsl_HAVE( TYPE_TRAITS ) && gsl_HAVE( VARIADIC_TEMPLATE )
 
 
 CASE( "conjunction<> and disjunction<>: Short-circuiting is handled correctly" )
 {
-#if gsl_HAVE( TYPE_TRAITS )
+#if gsl_HAVE( TYPE_TRAITS ) && gsl_HAVE( VARIADIC_TEMPLATE )
     static_assert(std::is_base_of<True<0>, std17::disjunction<True<0>, Incomplete>>::value, "static assertion failed");
     static_assert(std::is_base_of<False<0>, std17::conjunction<False<0>, Incomplete>>::value, "static assertion failed");
-#endif // gsl_HAVE( TYPE_TRAITS )
+#endif // gsl_HAVE( TYPE_TRAITS ) && gsl_HAVE( VARIADIC_TEMPLATE )
 }
 
 CASE( "conjunction<> and disjunction<>: First suitable type is chosen as base" )
 {
-#if gsl_HAVE( TYPE_TRAITS )
+#if gsl_HAVE( TYPE_TRAITS ) && gsl_HAVE( VARIADIC_TEMPLATE )
     static_assert(std::is_base_of<std::false_type, std17::disjunction<>>::value, "static assertion failed");
     static_assert(std::is_base_of<True<0>, std17::disjunction<True<0>>>::value, "static assertion failed");
     static_assert(std::is_base_of<False<0>, std17::disjunction<False<0>>>::value, "static assertion failed");
@@ -53,5 +53,5 @@ CASE( "conjunction<> and disjunction<>: First suitable type is chosen as base" )
     static_assert(std::is_base_of<True<2>, std17::conjunction<True<0>, True<1>, True<2>>>::value, "static assertion failed");
     static_assert(std::is_base_of<False<1>, std17::conjunction<True<0>, False<1>, False<2>>>::value, "static assertion failed");
     static_assert(std::is_base_of<False<0>, std17::conjunction<False<0>, False<1>, False<2>>>::value, "static assertion failed");
-#endif // gsl_HAVE( TYPE_TRAITS )
+#endif // gsl_HAVE( TYPE_TRAITS ) && gsl_HAVE( VARIADIC_TEMPLATE )
 }
