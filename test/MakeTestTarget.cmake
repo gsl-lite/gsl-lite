e206392ecb8cd9dab2f6473ca17dca54ffe8941b
Moritz Beutel <mbeutel@users.noreply.github.com>
2023-04-08 23:44:57 +0200

    Enable "-Wweak-vtables" warning for Clang

--- test/MakeTestTarget.cmake
+++ test/MakeTestTarget.cmake
@@ -257,7 +257,10 @@ function( make_test_target target )
                 list( APPEND localOptions "-Wno-error=array-bounds" ) # work around compiler bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100137
             endif()
         elseif( CMAKE_CXX_COMPILER_ID MATCHES "Clang|AppleClang" )
-            list( APPEND localOptions "-Wno-c++11-long-long" ) # irrelevant strict-C++98 warning about non-standard type `long long`
+            list( APPEND localOptions
+                "-Wno-c++11-long-long"  # irrelevant strict-C++98 warning about non-standard type `long long`
+                "-Wweak-vtables"  # leads to warnings that need to be suppressed, cf. https://github.com/gsl-lite/gsl-lite/issues/322
+            )
         endif()
     endif()
 
