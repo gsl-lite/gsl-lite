ed77f88c3ff751eb3ac43ac58b62539294211d11
Moritz Beutel <mbeutel@users.noreply.github.com>
2023-06-07 16:52:10 +0200

    Clean up a little, and disable `not_null<void*>` test for C++98.

--- test/not_null.t.cpp
+++ test/not_null.t.cpp
@@ -1603,11 +1603,9 @@ CASE( "not_null<>: Able to deduce element_type of user-defined smart pointers ev
 #endif
 }
 
-static constexpr bool b1 = std::is_convertible_v<void*, int*>;
-static constexpr bool b2 = std::is_constructible_v<int*, void*>;
-
 CASE( "not_null<>: Can handle void*" )
 {
+#if gsl_HAVE( TYPE_TRAITS )  // we do not support `void*` without type traits available
     int i = 42;
     void * vp = &i;
 
@@ -1638,6 +1636,7 @@ CASE( "not_null<>: Can handle void*" )
     //gsl::not_null< int * > nip2 = static_cast< gsl::not_null< int * > >( nvp );
     //gsl::not_null< int * > nip2 = gsl::not_null< int * >( nvp );
     //int * ip = static_cast< int * >( nvp );
+#endif // gsl_HAVE( TYPE_TRAITS )
 }
 
 #if gsl_HAVE( HASH )
