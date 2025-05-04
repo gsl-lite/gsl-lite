4c047f5635e70725543c45cf50bfe6c827a1f82c
Moritz Beutel <mbeutel@users.noreply.github.com>
2025-05-04 23:32:55 +0200

    Update copyright year

--- include/gsl/gsl-lite.hpp
+++ include/gsl/gsl-lite.hpp
@@ -3,7 +3,7 @@
 // For more information see https://github.com/gsl-lite/gsl-lite
 //
 // Copyright (c) 2015-2019 Martin Moene
-// Copyright (c) 2019-2024 Moritz Beutel
+// Copyright (c) 2019-2025 Moritz Beutel
 // Copyright (c) 2015-2018 Microsoft Corporation. All rights reserved.
 //
 // This code is licensed under the MIT License (MIT).
