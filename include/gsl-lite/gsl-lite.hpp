8481734f2dea3f0dbdbbfc164807c495a7795024
Moritz Beutel <mbeutel@users.noreply.github.com>
2025-05-07 22:45:52 +0200

    Revert the tentative change to `gsl_ENABLE_IF_()`

--- include/gsl-lite/gsl-lite.hpp
+++ include/gsl-lite/gsl-lite.hpp
@@ -1245,16 +1245,11 @@
 #endif // gsl_HAVE( TYPE_TRAITS )
 
 #if gsl_HAVE( TYPE_TRAITS ) && gsl_HAVE( DEFAULT_FUNCTION_TEMPLATE_ARG )
-# if !gsl_BETWEEN( gsl_COMPILER_MSVC_VERSION, 1, 140 ) // VS 2013 seems to have trouble with SFINAE for default non-type arguments
-#  define gsl_ENABLE_IF_(VA) , typename std::enable_if< ( VA ), int >::type = 0
+# if ! gsl_BETWEEN( gsl_COMPILER_MSVC_VERSION, 1, 140 ) // VS 2013 seems to have trouble with SFINAE for default non-type arguments
+#  define gsl_ENABLE_IF_(VA)        , typename std::enable_if< ( VA ), int >::type = 0
 # else
-#  define gsl_ENABLE_IF_(VA) , typename = typename std::enable_if< ( VA ), ::gsl_lite::detail::enabler >::type
+#  define gsl_ENABLE_IF_(VA)        , typename = typename std::enable_if< ( VA ), ::gsl_lite::detail::enabler >::type
 # endif
-//# if !gsl_BETWEEN( gsl_COMPILER_MSVC_VERSION, 1, 140 ) // VS 2013 seems to have trouble with SFINAE for default non-type arguments
-#  define gsl_ENABLE_IF_(VA) , typename std::enable_if< ( VA ), int >::type = 0
-//# else
-//#  define gsl_ENABLE_IF_(VA) , typename = typename std::enable_if< ( VA ), ::gsl_lite::detail::enabler >::type
-//# endif
 #else
 # define  gsl_ENABLE_IF_(VA)
 #endif
