c8aef6b5115b244953a173bdf70c5e1c1bff2fbf
Moritz Beutel <mbeutel@users.noreply.github.com>
2026-08-19 17:42:11 +0200

    Minor comment change

--- test/util.t.cpp
+++ test/util.t.cpp
@@ -569,7 +569,7 @@ CASE( "gsl_DEFINE_ENUM_BITMASK_OPERATORS(): Allow explicit conversion to bool" )
     if ( flag2 & MyFlags::MyFlag1 ) { /* fine */ } else { EXPECT( false ); }
     if ( !( flag2 & MyFlags::MyFlag2 ) ) { /* fine */ } else { EXPECT( false ); }
 # if gsl_HAVE( AUTO )
-    //if ( flag1 | MyFlags::MyFlag1 )  // does not compile: supporting this would encourage mistakes (`|` instead of `&`)
+    //if ( flag1 | MyFlags::MyFlag1 ) { }  // does not compile: supporting this would encourage mistakes (`|` instead of `&`)
     static_assert( ! std::is_convertible< decltype( flag1 | MyFlags::MyFlag1 ), bool >::value, "static assertion failed" );
 # endif // gsl_HAVE( AUTO )
 #if defined( __cpp_consteval )
@@ -577,10 +577,10 @@ CASE( "gsl_DEFINE_ENUM_BITMASK_OPERATORS(): Allow explicit conversion to bool" )
     if ( flag2 != 0 ) { /* fine */ } else { EXPECT( false ); }
     if ( ( flag2 & MyFlags::MyFlag1 ) != 0 ) { /* fine */ } else { EXPECT( false ); }
     if ( ( flag2 & MyFlags::MyFlag2 ) == 0 ) { /* fine */ } else { EXPECT( false ); }
-    //if ( flag1 == 1 )  // does not compile
-    //if ( flag2 != 1 )  // does not compile
-    //if ( ( flag2 & MyFlags::MyFlag1 ) != 1 )  // does not compile
-    //if ( ( flag2 & MyFlags::MyFlag2 ) == 1 )  // does not compile
+    //if ( flag1 == 1 ) { }  // does not compile
+    //if ( flag2 != 1 ) { }  // does not compile
+    //if ( ( flag2 & MyFlags::MyFlag1 ) != 1 ) { }  // does not compile
+    //if ( ( flag2 & MyFlags::MyFlag2 ) == 1 ) { }  // does not compile
 #endif // defined( __cpp_consteval )
 # if gsl_HAVE( AUTO )
     //if ( ~flag1 )  // does not compile: supporting this would encourage mistakes (`~` instead of `!`)
